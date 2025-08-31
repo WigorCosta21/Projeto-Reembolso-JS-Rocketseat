feecfbaa1c63afa3563bcf33bde04e1d528bd3b1
Wigor <wigor21@hotmail.com>
2025-08-31 18:44:09 -0300

    feat: add form submit handler to capture and log new expense data

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, "")
@@ -15,4 +18,19 @@ const formatCurrencyBRL = (value) => {
     })
 
     return value
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    const newExpenses = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amout: amount.value,
+        created_at: new Date()
+    }
+
+    console.log(newExpenses)
 }
