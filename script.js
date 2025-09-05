9c8e8304008321a0ab7cd5161e6655fcd332a03c
Wigor <wigor21@hotmail.com>
2025-09-04 22:14:17 -0300

    feat: Implement update expense total calculation function

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 const expenseList = document.querySelector("ul")
+const expenseQuantity = document.querySelector("aside header p span")
+const expenseTotal = document.querySelector("aside header h2")
 
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, "")
@@ -68,9 +70,48 @@ const expenseAdd = (newExpense) => {
         expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
         expenseList.append(expenseItem)
 
+        updateTotals()
+
     }
     catch (error) {
         alert("Não foi possível atualizar a lista de despesas.")
         console.log(error)
     };
+}
+
+const updateTotals = () => {
+    try {
+        const items = expenseList.children
+
+        expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+        let total = 0
+
+        for (let item = 0; item < items.length; item++) {
+            const itemAmount = items[item].querySelector(".expense-amount")
+
+            let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",", ".")
+
+            value = parseFloat(value)
+
+            if (isNaN(value)) {
+                return alert("Não foi possível calcular o total. O valor não parece ser um número")
+            }
+
+            total += Number(value)
+        }
+
+        const symbolBRL = document.createElement("small")
+        symbolBRL.textContent = "R$"
+
+        total = formatCurrencyBRL(total).toUpperCase().replace("R$", "")
+
+        expenseTotal.innerHTML = ""
+
+        expenseTotal.append(symbolBRL, total)
+
+    } catch (error) {
+        console.log(error)
+        alert("Não foi possível atualizar os totais.")
+    }
 }
